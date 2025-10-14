0a5f59478bbe2c8238ce563f49ff8dbfe631ebbd
Danny <thanhdanh.ute2002@gmail.com>
2025-10-15 02:52:34 +0700

    update kickstart

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -12,6 +12,48 @@ vim.keymap.set('n', '<leader>ag', function()
 end, { desc = 'avante: toggle my prompt' })
 return {
   'yetone/avante.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'stevearc/dressing.nvim', -- for input provider dressing
+    'folke/snacks.nvim', -- for input provider snacks
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+      keys = {
+        -- suggested keymap
+        { '<leader>p', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   -- ⚠️ must add this setting! ! !
   build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or 'make',
@@ -281,8 +323,8 @@ return {
       override_timeoutlen = 500,
     },
     suggestion = {
-      debounce = 600,
-      throttle = 600,
+      debounce = 1000,
+      throttle = 1000,
     },
   },
   highlights = {
@@ -455,7 +497,7 @@ return {
   },
   --- @class AvanteRepoMapConfig
   repo_map = {
-    ignore_patterns = { '%.git', '%.worktree', '__pycache__', 'node_modules' }, -- ignore files matching these
+    ignore_patterns = { '%.git', '%.worktree', '__pycache__', 'node_modules', '%.DS_Store', '%.png', '%.jpg', '%.jpeg', '%.gif', '%.svg', '%.ico', '%.woff', '%.woff2', '%.ttf', '%.eot', '%.wasm', '%.node', 'js-debug', 'vscode-chrome-debug' }, -- ignore files matching these
     negate_patterns = {}, -- negate ignore files matching these.
   },
   --- @class AvanteFileSelectorConfig
@@ -476,8 +518,8 @@ return {
     provider_opts = {},
   },
   suggestion = {
-    debounce = 600,
-    throttle = 600,
+    debounce = 1000,
+    throttle = 1000,
   },
   disabled_tools = {}, ---@type string[]
   ---@type AvanteLLMToolPublic[] | fun(): AvanteLLMToolPublic[]
@@ -486,42 +528,4 @@ return {
   slash_commands = {},
   ---@type AvanteShortcut[]
   shortcuts = {},
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
-    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    'stevearc/dressing.nvim', -- for input provider dressing
-    'folke/snacks.nvim', -- for input provider snacks
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua', -- for providers='copilot'
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
-      },
-      ft = { 'markdown', 'Avante' },
-    },
-  },
 }
